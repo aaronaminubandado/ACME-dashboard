60a8b6685815b76931ecd73a547afb66feed5a7a
badguybandi <aaron.aminu1@gmail.com>
2024-07-20 21:15:17 +0300

    Added the metadata for the pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { title } from 'process';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+	template: '%s | Acme  Dashboard',
+	default: 'Acme Dashboard',
+	},
+  description: 'Dashboard built with App Router',
+  metadataBase: new URL('http://next-learn-dashboard.vercel.sh'),
+}
 
 export default function RootLayout({
   children,
