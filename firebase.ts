6db69510091416fa2c920cf6308fa5e2f4fccb8d
badguybandi <aaron.aminu1@gmail.com>
2024-08-09 10:42:33 +0300

    added firebase app information

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD2b7oSkVlbZtl8jsMKLWBIS-DBvuuvbQA",
+  authDomain: "nextjs-dashboard-9ffc3.firebaseapp.com",
+  projectId: "nextjs-dashboard-9ffc3",
+  storageBucket: "nextjs-dashboard-9ffc3.appspot.com",
+  messagingSenderId: "281398546140",
+  appId: "1:281398546140:web:8d8be287e6531388b96e19",
+  measurementId: "G-MSDSXKLTJ8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
