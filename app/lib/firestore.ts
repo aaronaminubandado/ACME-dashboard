e41dfd0713cf570426935f79e779ecbc5a6f5e30
badguybandi <aaron.aminu1@gmail.com>
2024-08-13 17:56:33 +0300

    Added firebase config manually to firestore.ts

--- app/lib/firestore.ts
+++ app/lib/firestore.ts
@@ -1,16 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { DocumentData, getFirestore, SnapshotOptions } from "firebase/firestore";
 
-// const firebaseConfig = {
-//     apiKey: process.env.FIRE_APIKEY,
-//     authDomain: process.env.FIRE_AUTHDOMAIN,
-//     projectId: process.env.FIRE_PROJECTID,
-//     storageBucket: process.env.FIRE_STORAGEBUCKET,
-//     messagingSenderId: process.env.FIRE_MESSAGINGSENDERID,
-//     appId: process.env.FIRE_APPID,  
-//     measurementId: process.env.FIRE_MEASUREMENTID 
-// };
-
 const firebaseConfig = {
     apiKey: "AIzaSyD2b7oSkVlbZtl8jsMKLWBIS-DBvuuvbQA",
     authDomain: "nextjs-dashboard-9ffc3.firebaseapp.com",
